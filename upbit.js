f31a6562792c33cc6eafeed4ca26a960a76b3e91
DESKTOP-6GRH10V\82108 <hiherlin@gmail.com>
2022-05-10 18:39:28 +0800

    up

--- upbit.js
+++ upbit.js
@@ -23,7 +23,7 @@ const server = app.listen("1111", () => {
 });
 //------------------------------------SERVER-----------------------------------------
 
-const bot = new Bot("SOL", 5, 110);
+const bot = new Bot("BTC", 5, 25);
 // client.use(express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
   const LOG = bot.Log;
