ae54aede58bcce15aad24018a0844593eaf0e8a9
DESKTOP-G4PJVDG\With_Drone <hiherlin@gmail.com>
2022-07-14 15:08:09 +0900

    working rapid

--- upbit.js
+++ upbit.js
@@ -10,16 +10,16 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 
 // 집
-const secretKey = "p4rWJsbKVvUk5QemMLm5KHIyjoqq81K5lrvnLUqx";
-const accessKey = "k2eJf0aqdpCSfm65RkwuWpUHpTROiGMqNLUmAxwW";
+// const secretKey = "p4rWJsbKVvUk5QemMLm5KHIyjoqq81K5lrvnLUqx";
+// const accessKey = "k2eJf0aqdpCSfm65RkwuWpUHpTROiGMqNLUmAxwW";
 // 사무실
-// const secretKey = 'tEYvH9OsmLeQIhXsusvq4sx23wzUA2csJcqTWofw';
-// const accessKey = 'nwFxXxXva11NH7YpH7gPjis7WiQeDHrWgVhDXs3F';
+const secretKey = 'tEYvH9OsmLeQIhXsusvq4sx23wzUA2csJcqTWofw';
+const accessKey = 'nwFxXxXva11NH7YpH7gPjis7WiQeDHrWgVhDXs3F';
 
 async function start() {
   const wemix = new Bot_macd("WEMIX",15,1)
   const sand = new Bot_macd("SAND",240,1)
-  const rapid = new Bot_rapid(1)
+  const rapid = new Bot_rapid(1.05)
   // wemix.play();
   // sand.play();
   rapid.play();
